794627347c17f71aa860154a66f76a173ab09ff3
Prince Asiedu <princekofasiedu@gmail.com>
2020-09-08 16:55:21 -0700

    Fixed state lifting issue between ArticleNav and ArticleBody

--- src/components/blog/Articles/ArticleComp/ArticlesBody.js
+++ src/components/blog/Articles/ArticleComp/ArticlesBody.js
@@ -1,23 +1,60 @@
 import React, { Component } from "react";
-import ArticleNav from './ArticleNav';
+import ArticleNav from "./ArticleNav";
+import Item from "./Items";
 import { ArticlesBox } from "./ArticlesBox";
+
 export class ArticlesBody extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      articles: "",
+      modified: "",
+      heading: "All",
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000/api/articles")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({ articles: data });
+      });
+  }
+  handleValueChange = (val) => {
+    console.log(`ArticlesBody: ${val}`);
+    this.setState({ heading: val });
+  };
+
   render() {
+    let DbArt =
+      this.state.articles.length > 0
+        ? this.state.articles.map((article) => {
+            return (
+              <Item
+                key={article._id}
+                name={article.title}
+                type={article.author}
+              />
+            );
+          })
+        : "undefined";
+
     return (
       <div>
-        {/* favourite section */}
+        <ArticleNav
+          handleClick={(id) => {
+            this.handleValueChange(id);
+          }}
+        />
 
-        {/* Navigation */}
-        <ArticleNav />
-        {/* Heading */}
         <div className="articles-body__heading">
-          <h2 className="articles-body__heading__text"> Any </h2>
+          <h2 className="articles-body__heading__text" style={{textTransform: 'capitalize'}} >{this.state.heading}</h2>
         </div>
 
         {/* General */}
-        <ArticlesBox>
-
-        </ArticlesBox>
+        <ArticlesBox>{DbArt}</ArticlesBox>
       </div>
     );
   }
