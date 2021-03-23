7dc5c417d4b3b953300faa407e4d0d643c267d86
Prince Asiedu <princekofasiedu@gmail.com>
2021-03-23 17:29:21 +0000

    Included HMR in index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab  } from "@fortawesome/free-brands-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import "react-calendar/dist/Calendar.css";
 import {
   faCheckSquare,
@@ -80,7 +80,10 @@ library.add(
   faLowVision,
   faAppleAlt,
   faSmile
-
 );
 
 ReactDom.render(<App />, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept();
+}
