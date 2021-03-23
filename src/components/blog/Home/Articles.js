7df338511add01798a55a03163c0620d47192574
Prince Asiedu <princekofasiedu@gmail.com>
2021-03-23 17:51:31 +0000

    Refactored Articles.js

--- src/components/blog/Home/Articles.js
+++ src/components/blog/Home/Articles.js
@@ -3,7 +3,7 @@ import Button from "../Util/Button";
 import Picture from "../Util/Picture";
 import { Link } from "react-router-dom";
 
-function Articles() {
+const Articles = () => {
   return (
     <section className="articles">
       <div className="articles__firstChild">
@@ -14,21 +14,26 @@ function Articles() {
           </h2>
         </div>
       </div>
-        <section className="articles__paragraph">
-          <p>
-            An amalgam of original articles written deep from within thoughts and emotions. They encapsulate a host of societal issues ranging from sexism, racism, misogynism, homophobia, transphobia and religion. Please feel free to read and engage with our thoughts with your likes and comments. It would mean a great deal to us if you'd share as well, thank you!
-          </p>
-          <section className="buttons">
-            <Link to="/articles">
-              <Button label="Articles" importance="medium" icon="angle-right" />
-            </Link>
-          </section>
+      <section className="articles__paragraph">
+        <p>
+          An amalgam of original articles written deep from within thoughts and
+          emotions. They encapsulate a host of societal issues ranging from
+          sexism, racism, misogynism, homophobia, transphobia and religion.
+          Please feel free to read and engage with our thoughts with your likes
+          and comments. It would mean a great deal to us if you'd share as well,
+          thank you!
+        </p>
+        <section className="buttons">
+          <Link to="/articles">
+            <Button label="Articles" importance="medium" icon="angle-right" />
+          </Link>
         </section>
+      </section>
 
-        <div className="articles-author">
-          <Picture />
-        </div>
+      <div className="articles-author">
+        <Picture />
+      </div>
     </section>
   );
-}
+};
 export default Articles;
