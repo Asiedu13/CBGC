c4bf8973d642e3ac1cacedb62b9ba9833059f68d
Prince Asiedu <princekofasiedu@gmail.com>
2021-03-23 17:44:33 +0000

    Refactored Nav.js

--- src/components/blog/Nav.js
+++ src/components/blog/Nav.js
@@ -2,12 +2,10 @@ import React from "react";
 import Logo from "./Nav/Logo";
 import Links from "./Nav/Links";
 
-function Nav() {
-  return (
-    <div className="nav">
-      <Logo />
-      <Links />
-    </div>
-  );
-}
+const Nav = () => {
+  <div className="nav">
+    <Logo />
+    <Links />
+  </div>;
+};
 export default Nav;
