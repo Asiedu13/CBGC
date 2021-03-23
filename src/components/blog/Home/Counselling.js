14bd8fbaf1f63e45e910ad36585c9b547a25d9c9
Prince Asiedu <princekofasiedu@gmail.com>
2021-03-23 17:52:43 +0000

    Refactored Counselling.js

--- src/components/blog/Home/Counselling.js
+++ src/components/blog/Home/Counselling.js
@@ -2,15 +2,11 @@ import React from "react";
 import Booking from "./CounsellingComp/Booking";
 import El from "./CounsellingComp/Elaboration";
 
-class Counselling extends React.Component {
-  render() {
-    return (
-      <section className="counselling">
-        <El />
-        <Booking />
-      </section>
-    );
-  }
-}
+const Counselling = () => (
+  <section className="counselling">
+    <El />
+    <Booking />
+  </section>
+);
 
 export default Counselling;
