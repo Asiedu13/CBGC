220f63536f73615d3bb55d29b6e531f82b32b664
Prince Asiedu <princekofasiedu@gmail.com>
2021-05-31 00:30:31 +0100

    Modified server.js port

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 app.use(cookieParser());
-app.use(session({
- secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX", 
- resave: true, 
- saveUninitialized: true 
-}));
+// app.use(session({
+//  secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX", 
+//  resave: true, 
+//  saveUninitialized: true 
+// }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
